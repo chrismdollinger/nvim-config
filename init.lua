708d41492e4ac1120f92a79bc8563c5762af2d42
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-02-07 13:30:12 -0500

    initial content commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("config.lazy")
+require("config.telescope")
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
+vim.cmd('let g:netrw_keepdir=0')
+vim.cmd('set nocp') -- 'compatible' is not set
+vim.cmd('filetype plugin on') -- plugins are enabled
