c5269d43e52dda41588b7f10d16af5c2dbfdba8e
Christopher Dollinger <chris@christophersmacbookpro.local>
2025-02-12 13:56:32 -0500

    Adding Telescope activation hotkeys

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -28,3 +28,9 @@ require('telescope').setup{
     -- please take a look at the readme of the extension you want to configure
   }
 }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', 'fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', 'fh', builtin.help_tags, { desc = 'Telescope help tags' })
