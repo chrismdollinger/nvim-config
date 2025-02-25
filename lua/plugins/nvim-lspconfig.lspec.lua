e847d5cdd18411587772e115b32039ad5ac1859c
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-02-25 13:18:57 -0500

    Setting up Lazy Plugin Spec
    Nvim-lspconfig spec works, Lazy is loading it.

---
+++ lua/plugins/nvim-lspconfig.lspec.lua
@@ -0,0 +1,8 @@
+-- Neovim LSP Config: https://github.com/neovim/nvim-lspconfig
+-- Updating by release from GitHub
+return {
+    {
+        "neovim/nvim-lspconfig",
+        version = "*"
+    },
+}
