08214e63b3ffb92cb989aafe44f0bff8ebbb1726
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-02-25 14:16:25 -0500

    Installing Telescope fuzzy finder
    It is highly extensible, hopefully it has everything I want and I can
    easily customize the rest

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Setting up dependencies and providers
 require("config.lazy")
 require("config.lsp")
+require("config.fuzzyfinder")
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
