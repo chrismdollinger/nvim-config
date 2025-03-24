be029af3d4cabd228e081f7eee6c74a4ce56bdaf
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-03-24 17:32:13 -0400

    Adding Catppucin

--- init.lua
+++ init.lua
@@ -18,4 +18,4 @@ vim.cmd('set shiftwidth=4')
 vim.cmd('let g:netrw_keepdir=0')
 vim.cmd('set nocp')
 
-vim.cmd("colorscheme evening")
+vim.cmd("colorscheme catppuccin-macchiato")
---
+++ lua/plugins/catppuccin.lspec.lua
@@ -0,0 +1,10 @@
+-- Catppuccin Repo: https://github.com/catppuccin/nvim
+-- Updating by release from GitHub
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        version = "*",
+        priority = 1000
+    },
+}
