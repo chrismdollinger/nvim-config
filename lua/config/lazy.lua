ef110250f9de8897ce4cde1bccf49a747f00cbcb
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-04-13 01:21:00 -0400

    Cleaning up checkhealth errors

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,4 +16,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        import = "plugins",
+    },
+    rocks = {
+        enabled = false,
+    },
+})
