f420a0df1cc7d45cb8d9e6d717ddad497c9a25de
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-04-14 10:53:20 -0400

    adding nushell LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,5 +5,6 @@ local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 lsp.csharp_ls.setup({ capabilities = capabilities })
 lsp.lua_ls.setup({ capabilities = capabilities })
+lsp.nushell.setup({ capabilities = capabilities })
 
 return M
