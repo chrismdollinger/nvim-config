b068ab60f951655bdab651033454a72d58f8af60
chrismdollinger <4001728+chrismdollinger@users.noreply.github.com>
2025-05-13 18:53:11 -0400

    adding css lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,6 +7,7 @@ lsp.csharp_ls.setup({ capabilities = capabilities })
 lsp.lua_ls.setup({ capabilities = capabilities })
 lsp.nushell.setup({ capabilities = capabilities })
 
+vim.lsp.enable('emmet_language_server')
 vim.lsp.enable("bashls");
 
 return M
